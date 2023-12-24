af0f20c0fec04df56d5944266b05703993b412a5
ldanh270 <ducanhle.dn@gmail.com>
2023-12-24 13:06:46 +0700

    Done add A.cpp + A.inp + A.out

---
+++ Cpp/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef string str;
+
+const ll mod = 1e9 + 7;
+const ll oo = 1e18;
+const ll N = 1e5;
+const ll AI = 1e6;
+
+void solve() {
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("A.inp", "r", stdin);
+    freopen("A.out", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll tcase = 1;
+    cin >> tcase;
+    while (tcase--) solve();
+}
